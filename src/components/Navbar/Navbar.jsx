c07cbd250ae5fede933a5ec3be07c3fb5891f467
unknown <gustavo.bordignon@outlook.com>
2024-01-09 14:36:45 -0300

    fix mobile navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -38,11 +38,11 @@ const Navbar = () => {
             <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
               <MdOutlineRestaurantMenu color='#fff' fontSize={27} onClick={() => setToggle(false)}  className='overlay__close'/>
               <ul className='app__navbar-smallscreen_links'>
-                <li className='p__opensans'><a href='#home'>Home</a></li>
-                <li className='p__opensans'><a href='#about'>Sobre</a></li>
-                <li className='p__opensans'><a href='#menu'>Menu</a></li>
-                <li className='p__opensans'><a href='#awards'>Premios</a></li>
-                <li className='p__opensans'><a href='#contact'>Contato</a></li>
+                <li className='p__opensans'><a href='#home' onClick={() => setToggle(false)}>Home</a></li>
+                <li className='p__opensans'><a href='#about' onClick={() => setToggle(false)}>Sobre</a></li>
+                <li className='p__opensans'><a href='#menu' onClick={() => setToggle(false)}>Menu</a></li>
+                <li className='p__opensans'><a href='#awards' onClick={() => setToggle(false)}>Premios</a></li>
+                <li className='p__opensans'><a href='#contact' onClick={() => setToggle(false)}>Contato</a></li>
               </ul>
           </div>
         )}
